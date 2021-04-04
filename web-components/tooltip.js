ff51d3da95c7c186b757644c27b88966dfd19765
Suranjan Dutta Moitra <suranjanbiswas2002@gmail.com>
2021-04-04 13:34:42 +0530

    Added the event listeners for tooltips

--- web-components/tooltip.js
+++ web-components/tooltip.js
@@ -1,7 +1,25 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super();
-    console.log("This is working!");
+    this.tooltipContainer;
+  }
+
+  connectedCallback() {
+    const tooltipIcon = document.createElement("span");
+    tooltipIcon.textContent = " (?)";
+    tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
+    tooltipIcon.addEventListener("mouseleave", this._removeTooltip.bind(this));
+    this.appendChild(tooltipIcon);
+  }
+
+  _showTooltip() {
+    this.tooltipContainer = document.createElement("div");
+    this.tooltipContainer.textContent = "This is the tooltip text!";
+    this.appendChild(this.tooltipContainer);
+  }
+
+  _removeTooltip() {
+    this.removeChild(this.tooltipContainer);
   }
 }
 
